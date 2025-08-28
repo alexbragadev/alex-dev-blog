f97bbf8f2af13e74e9f5cb11b2f0d2e294d5a7b6
AlexAraujoDeBraga <alexbraga107@gmail.com>
2025-08-28 20:03:03 -0300

    config webpack

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const app = express();
 const port = 3000;
 
 // Servir arquivos da pasta public (css/js/etc.)
+app.use(express.static(path.join(__dirname, '..')));
+// Servir arquivos da raiz do projeto
 app.use('/public', express.static(path.join(__dirname, '../public')));
 
 // Servir index.html da raiz manualmente
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './app.js', // ← usando app.js como ponto de entrada
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/js'), // ← gera bundle aqui
+    publicPath: '/js/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'], // para importar CSS (como Bootstrap)
+      },
+    ],
+  },
+};
